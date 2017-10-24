4d3204ebace560f6af0d2a5c421a687beff4aa00
kirilvuchkov <kirilvuchkov@gmail.com>
2017-10-24 07:11:08 +0300

    lab3 solved

--- lab3/1.cpp
+++ lab3/1.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    float x, y;
+    
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " / " << y << " = " << x / y << endl;
+
     return 0;
 }
--- lab3/2.cpp
+++ lab3/2.cpp
@@ -5,5 +5,82 @@ using namespace std;
 
 int main() {
 
+    // Declaration
+    const int LEVA = 100;
+
+    int total;
+    int paid;
+    int change;
+    int left;
+    int hundreds;
+    int fifties;
+    int twenties;
+    int tens;
+    int ones;
+    int fiftyCents;
+    int twentyCents;
+    int tenCents;
+    int fiveCents;
+    int twoCents;
+    int oneCents;
+
+    // Input
+    cout << "Total: ";
+    cin >> total;
+    cout << "Paid: ";
+    cin >> paid;
+
+    // Calculations
+    left = change = paid - total;
+
+    hundreds = left / (100 * LEVA);
+    left = left % (100 * LEVA);
+
+    fifties = left / (50 * LEVA);
+    left = left % (50 * LEVA);
+
+    twenties = left / (20 * LEVA);
+    left = left % (20 * LEVA);
+
+    tens = left / (10 * LEVA);
+    left = left % (10 * LEVA);
+
+    ones = left / (1 * LEVA);
+    left = left % (1 * LEVA);
+
+    fiftyCents = left / 50;
+    left = left % 50;
+
+    twentyCents = left / 20;
+    left = left % 20;
+
+    tenCents = left / 10;
+    left = left % 10;
+
+    fiveCents = left / 5;
+    left = left % 5;
+
+    twoCents = left / 2;
+    left = left % 2;
+
+    oneCents = left;
+
+    // Format Configuration
+    cout << fixed << setprecision(2);
+
+    // Output
+    cout << "Change: " << change / (float) LEVA << endl;
+    cout << hundreds << " x 100" << endl;
+    cout << fifties << " x 50" << endl;
+    cout << twenties << " x 20" << endl;
+    cout << tens << " x 10" << endl;
+    cout << ones << " x 1" << endl;
+    cout << fiftyCents << " x 50 cents" << endl;
+    cout << twentyCents << " x 20 cents" << endl;
+    cout << tenCents << " x 10 cents" << endl;
+    cout << fiveCents << " x 5 cents" << endl;
+    cout << twoCents << " x 2 cents" << endl;
+    cout << oneCents << " x 1 cent" << endl;
+
     return 0;
 }
